4f326166b0eba5f01afc4f337286182d5b98569a
Hannes Rist <github@mirsin.org>
2015-01-11 00:44:40 +0100

    initial working commit

---
+++ README.md
@@ -0,0 +1,6 @@
+===== File UPload =====
+
+Create a directory `downloads'.
+Compile it.
+Run it.
+Open a webbrowser to `http://localhost:8080'
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	//"fmt"
+	//"io/ioutil"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type page struct {
+	Title string
+	Body  []byte
+}
+
+func main() {
+	http.Handle("/downloads/", http.StripPrefix("/downloads/", http.FileServer(http.Dir("./downloads"))))
+	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/", doRest)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		log.Println("Kabutt: ", err)
+	}
+	defer file.Close()
+
+	out, err := os.Create("./downloads/" + header.Filename)
+	defer out.Close()
+	if err != nil {
+		log.Println("Kabutt: ", err)
+	}
+	_, err = io.Copy(out, file)
+	if err != nil {
+		log.Println("Kabutt: ", err)
+	}
+
+	http.Redirect(w, r, "/downloads", http.StatusFound)
+
+}
+func doRest(w http.ResponseWriter, r *http.Request) {
+	p := &page{Title: "File UPload"}
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, p)
+}
